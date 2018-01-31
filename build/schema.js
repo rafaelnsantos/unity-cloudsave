e3c0937b9e5ec9ac565c2cc0b4d45a607f10aeeb
Rafael Santos <rafaelnsantos1@gmail.com>
2018-01-31 17:48:18 -0200

    teste nove_env

--- build/schema.js
+++ build/schema.js
@@ -12,7 +12,7 @@ var _schemaglue = require('schemaglue');
 
 var transpileSchema = _graphqlS2s.graphqls2s.transpileSchema; // https://hackernoon.com/graphql-schemas-easier-better-structure-31677a640d18
 
-var _glue = (0, _schemaglue.glue)(process.env.DEV ? 'src/graphql' : 'build/src/graphql'),
+var _glue = (0, _schemaglue.glue)(process.env.NODE_ENV !== 'production' ? 'src/graphql' : 'build/src/graphql'),
     schema = _glue.schema,
     resolver = _glue.resolver;
 
