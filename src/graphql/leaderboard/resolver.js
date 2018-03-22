90d03a426538a702ba988a39d51734630bdaeaa2
Rafael Santos <rafaelnsantos1@gmail.com>
2018-03-22 15:18:48 -0300

    PublicLeaderboard

--- src/graphql/leaderboard/resolver.js
+++ src/graphql/leaderboard/resolver.js
@@ -1,7 +1,12 @@
+import UserModel from '@/models/user'
+
 exports.resolver = {
 	Query: {
 		Leaderboard (root, {top, key}, context) {
 			return context.user.GetLeaderboard(top, key)
+		},
+		PublicLeaderboard (root, {appid, key}) {
+			return UserModel.Leaderboard(appid, key)
 		}
 	}
 }
