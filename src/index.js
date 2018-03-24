dd9a78959e811933d7f5809858cf4dc5912b7449
Rafael Santos <rafaelnsantos1@gmail.com>
2018-03-24 14:51:17 -0300

    fix code smell

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { graphqlExpress, graphiqlExpress } from 'graphql-server-express'
+import { graphqlExpress } from 'graphql-server-express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import schema from './schema'
@@ -7,22 +7,31 @@ import context from 'context-middleware'
 import middlewares from '@/middlewares'
 import throng from 'throng'
 
+// variavel usada para determinar quantidade de instancias
 var WORKERS = process.env.WEB_CONCURRENCY || 1
 
 function startApp () {
+	// connect db
 	require('@/config/db')
 
 	const app = express()
 
+	// insert context in req
 	app.use(context())
 
-	app.use(bodyParser.raw({ type: 'application/memorycloud.custom-type' }))
+	// CORS
+	app.use(cors())
 
-	// .use all middlewares from the folder
+	// insert body in req
+	app.use(bodyParser.raw({ type: 'application/memorycloud.custom-type' }))
+	app.use(bodyParser.json()) // used for tests with graphiQL app
+
+	// app.use() all middlewares from the folder
 	Object.keys(middlewares).map(e => app.use(middlewares[e]))
 
+	// graphql endpoint
 	app.use('/graphql',
-		bodyParser.json(), cors(), graphqlExpress((req) => ({
+		graphqlExpress((req) => ({
 			schema,
 			context: req.context
 		}))
@@ -32,6 +41,7 @@ function startApp () {
 	app.listen(process.env.PORT, () => console.log('Listening at port', process.env.PORT))
 }
 
+// throng used for scaling when in production
 if (process.env.NODE_ENV === 'production') {
 	throng(WORKERS, startApp)
 } else {
